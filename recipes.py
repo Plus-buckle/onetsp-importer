a0654d8d9fa0fdb334617dd7784030a17badc7b6
Plus-buckle <plusbuckle@exactzero.net>
2026-06-20 20:07:52 -0500

    Code to match description

--- recipes.py
+++ recipes.py
@@ -12,38 +12,49 @@ recipes = []
 for thisfile in files:
     with open(os.path.join(dir_path, thisfile), "r") as file:
         current = {}
+        desc_done = False
         while True:
             line = file.readline()
             if "Recipe exported from One tsp" in line:
                 continue
             if line != "\n" and not current.get("title", False):
                 current["title"] = line.rstrip("\n")
+                continue
             if line.startswith("Yield:"):
                 clean_line = line.removeprefix("Yield: ")
                 clean_line = clean_line.rstrip("\n")
                 current["yield"] = clean_line
+                continue
             if line.startswith("Prep time:"):
                 clean_line = line.removeprefix("Prep time: ")
                 clean_line = clean_line.rstrip("\n")
                 current["prep"] = clean_line
+                continue
             if line.startswith("Cooking time:"):
                 clean_line = line.removeprefix("Cooking time: ")
                 clean_line = clean_line.rstrip("\n")
                 current["cook"] = clean_line
+                continue
             if line.startswith("Total time:"):
                 clean_line = line.removeprefix("Total time: ")
                 clean_line = clean_line.rstrip("\n")
                 current["total"] = clean_line
+                continue
             if line.startswith("URL:"):
                 clean_line = line.removeprefix("URL: ")
                 clean_line = clean_line.rstrip("\n")
                 current["url"] = clean_line
+                continue
+            if "INGREDIENTS" in line:
+                desc_done = True
+                continue
+            if line != "\n" and not desc_done:
+                current["desc"] = ""
+                desc_done = True
+                while line != "\n":
+                    current["desc"] += line
+                    line = file.readline()
             if not line:
-                print(current)
                 recipes.append(current)
                 break  # Stop when end of file is reached
-# open current file
-# read a line at a time
-# try to match something
-# end loop
 pprint.pprint(recipes)
